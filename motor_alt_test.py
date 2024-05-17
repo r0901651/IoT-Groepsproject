b180f89a2fa3128ce3a4f1d8342434739c465312
Anthony Van Roy <95848828+r0901651@users.noreply.github.com>
2024-05-17 17:25:02 +0200

    Add files via upload

---
+++ motor_alt_test.py
@@ -0,0 +1,55 @@
+import RPi.GPIO as GPIO
+import time
+
+# Define GPIO pins connected to the ULN2003AN driver board
+IN1 = 17
+IN2 = 27
+IN3 = 22
+IN4 = 26
+
+STEPS_DEG = 48
+
+# Define the sequence of steps for counterclockwise rotation (reversed)
+STEP_SEQUENCE_COUNTCLOCKWISE = [
+    [0, 0, 0, 1],
+    [0, 0, 1, 1],
+    [0, 0, 1, 0],
+    [0, 1, 1, 0],
+    [0, 1, 0, 0],
+    [1, 1, 0, 0],
+    [1, 0, 0, 0],
+    [1, 0, 0, 1]
+]
+
+def setup_motor():
+    GPIO.setmode(GPIO.BCM)  # Use BCM numbering
+    GPIO.setup(IN1, GPIO.OUT)
+    GPIO.setup(IN2, GPIO.OUT)
+    GPIO.setup(IN3, GPIO.OUT)
+    GPIO.setup(IN4, GPIO.OUT)
+
+def set_step_countclock(step):
+    GPIO.output(IN1, STEP_SEQUENCE_COUNTCLOCKWISE[step][0])
+    GPIO.output(IN2, STEP_SEQUENCE_COUNTCLOCKWISE[step][1])
+    GPIO.output(IN3, STEP_SEQUENCE_COUNTCLOCKWISE[step][2])
+    GPIO.output(IN4, STEP_SEQUENCE_COUNTCLOCKWISE[step][3])
+
+def rotate_counterclockwise(degrees):
+    steps = int(degrees / 30) * STEPS_DEG
+    for _ in range(steps):
+        for step in range(8):
+            set_step_countclock(step)
+            time.sleep(0.01)
+
+def cleanup():
+    GPIO.cleanup()
+
+if __name__ == '__main__':
+    try:
+        setup_motor()
+        rotate_counterclockwise(30)  # Rotate 30 degrees counterclockwise
+    except KeyboardInterrupt:
+        print("\nProgram stopped by user.")
+    finally:
+        cleanup()
+        
